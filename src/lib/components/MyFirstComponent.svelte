760ac246162f960374e8d4d3b34e994b6794d091
Arely Miramontes Rodriguez <arelymts@gmail.com>
2024-10-08 17:53:45 -0500

    Add basic component

---
+++ src/lib/components/MyFirstComponent.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="rounded-lg bg-gray-100 p-4 shadow-lg">
+	<h2 class="text-xl font-bold">{title}</h2>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import MyComponent from '../lib/components/MyFirstComponent.svelte';
+</script>
+
+<div>
+	<h1>ArelySkywalker</h1>
+	<MyComponent title="Hello World!" />
+</div>
